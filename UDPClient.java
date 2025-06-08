9de5ec501fda56dffd0219b32ea531b057caabc5
RayTracingON <aipaixz@gmail.com>
2025-06-08 14:58:31 +0800

    for space

--- UDPClient.java
+++ UDPClient.java
@@ -31,11 +31,10 @@ public class UDPClient {
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
             clientSocket.send(sendPacket);
             System.out.println("Message sent to server.");
-            byte[] receiveData = new byte[1024];
+            byte[] receiveData = new byte[2048];
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             clientSocket.receive(receivePacket);
             String serverResponse = new String(receivePacket.getData(), 0, receivePacket.getLength());
-            System.out.println("Response from server: " + serverResponse);
             if (serverResponse.startsWith("ERR")) {
                 System.err.println("Server returned an error: " + serverResponse);
                 continue; 
@@ -46,13 +45,22 @@ public class UDPClient {
                 continue;
             }
             System.out.println("Received from server: '" + serverResponse + "'");
-            String[] parts = serverResponse.split(" ");
-            long fileSize = Long.parseLong(parts[3]);
-            int dataPort = Integer.parseInt(parts[5]); 
-            serverResponse.split(" ");
-            String filename = parts[1];
+            String sizeKeyword = " SIZE ";
+            String portKeyword = " PORT ";
 
-            System.out.println("Starting file download from port " + dataPort + "...");
+            int sizeIndex = serverResponse.indexOf(sizeKeyword);
+            int portIndex = serverResponse.indexOf(portKeyword);
+
+            String filename = serverResponse.substring(3, sizeIndex).trim();
+        
+            String sizeStr = serverResponse.substring(sizeIndex + sizeKeyword.length(), portIndex).trim();
+            long fileSize = Long.parseLong(sizeStr);
+
+            String portStr = serverResponse.substring(portIndex + portKeyword.length()).trim();
+            int dataPort = Integer.parseInt(portStr);
+        
+            System.out.println("Successfully parsed response. Filename: '" + filename + "', Size: " + fileSize + ", New Port: " + dataPort);            
+
             try (FileOutputStream fos = new FileOutputStream("downloaded_" + filename)) {
                 long bytesReceived = 0;
                 int chunkSize = 1000; 
@@ -70,14 +78,21 @@ public class UDPClient {
                     String dataResponse = new String(receivePacket.getData(), 0, receivePacket.getLength());
                     
                     // "FILE <filename> OK START <start> END <end> DATA <base64_data>"
-                    String[] dataParts = dataResponse.split(" ", 7);
-                    if(dataParts.length == 7 && dataParts[0].equals("FILE") && dataParts[6] != null) {
-                        byte[] decodedData = Base64.getDecoder().decode(dataParts[6]);
-                        fos.write(decodedData);
-                        bytesReceived += decodedData.length;
-                        System.out.printf("Received chunk. Total downloaded: %d / %d bytes (%.2f%%)\n", bytesReceived, fileSize, (double)bytesReceived / fileSize * 100);
-                    } else {
-                        System.err.println("Received corrupted or invalid data packet.");
+                    String dataHeader = " DATA ";
+                    int dataIndex = dataResponse.indexOf(dataHeader);
+
+                    if (dataIndex != -1) {
+                    // 提取 " DATA " 关键字之后的所有内容
+                    String base64Data = dataResponse.substring(dataIndex + dataHeader.length());
+    
+                    // 现在 base64Data 是纯净的Base64字符串，可以安全地解码
+                    byte[] decodedData = Base64.getDecoder().decode(base64Data);
+                    fos.write(decodedData);
+                    bytesReceived += decodedData.length;
+                    System.out.printf("Received chunk. Total downloaded: %d / %d bytes (%.2f%%)\n", bytesReceived, fileSize, (double)bytesReceived / fileSize * 100);
+                    } 
+                    else {
+                        System.err.println("Received corrupted or invalid data packet: " + dataResponse);
                     }
 
 
